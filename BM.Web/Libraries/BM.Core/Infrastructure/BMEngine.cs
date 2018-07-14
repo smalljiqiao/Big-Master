f9bf2a2b8e4dc1236b271d7fcc5c0d35a387d821
MatueXL <1814180611@163.com>
2018-07-14 10:53:51 +0800

    添加启动任务和依赖注入相关类

---
+++ BM.Web/Libraries/BM.Core/Infrastructure/BMEngine.cs
@@ -0,0 +1,77 @@
+﻿using BM.Core.Infrastructure.DependencyManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace BM.Core.Infrastructure
+{
+    public class BMEngine : IEngine
+    {
+        private ContainerManager _containerManager;
+
+        protected virtual void RunStartupTasks()
+        {
+            var typeFinder = _containerManager.Resolve<ITypeFinder>();
+            var startupTaskTypes = typeFinder.FindClassesOfType<IStartupTask>();
+            var startupTasks = startupTaskTypes.Select(startupTaskType => (IStartupTask) Activator.CreateInstance(startupTaskType)).ToList();
+
+            //sort
+            startupTasks = startupTasks.AsQueryable().OrderBy(s => s.Order).ToList();
+            foreach(var startupTask in startupTasks)
+                startupTask.Execute();
+        }
+
+        protected virtual void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+
+            //dependencies
+            var typeFinder = new WebAppTypeFinder();
+            var drTypes = typeFinder.FindClassesOfType<IDependencyRegistrar>();
+            var drInstances = drTypes.Select(drType => (IDependencyRegistrar) Activator.CreateInstance(drType)).ToList();
+
+            //sort
+            drInstances = drInstances.AsQueryable().OrderBy(s => s.Order).ToList();
+            foreach(var dependencyRegistrar in drInstances)
+                dependencyRegistrar.Register(builder,typeFinder);
+
+            var container = builder.Build();
+            this._containerManager = new ContainerManager(container);
+
+            //MVC中实现DI的类，例如控制器的注入等。
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        public void Initialize()
+        {
+            //依赖注入
+            RegisterDependencies();
+
+            //运行启动任务
+            RunStartupTasks();
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            return ContainerManager.Resolve<T>();
+        }
+
+        public object Resolve(Type type)
+        {
+            return ContainerManager.Resolve(type);
+        }
+
+        public T[] ResolveAll<T>()
+        {
+            return ContainerManager.ResolveAll<T>();
+        }
+
+        public ContainerManager ContainerManager
+        {
+            get => _containerManager;
+        }
+    }
+}
---
+++ BM.Web/Libraries/BM.Core/Infrastructure/Singleton.cs
@@ -0,0 +1,11 @@
+﻿namespace BM.Core.Infrastructure
+{
+    /// <summary>
+    /// 单例模式泛型类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class Singleton<T>
+    {
+        public static T Instance { get; set; }
+    }
+}
