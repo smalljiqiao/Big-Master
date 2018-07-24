2c429226070ea4e6a39b3502d8656b5037d38ee2
1134774872@qq.com <jr88695>
2018-07-25 00:42:58 +0800

    优化baby

--- BM.Web/BigMasterApp/Content/Js/bmAPI.js
+++ BM.Web/BigMasterApp/Content/Js/bmAPI.js
@@ -4,6 +4,10 @@ var bmApI = {
         register: global_api_root + "user/register",
         login: global_api_root + "user/login",
         updatePwd: global_api_root +"user/changepassword",
+    },
+    fortunDream: {
+        getTitle: global_api_root + "fortun/dream/title",//
+        itemDetail: global_api_root +"fortun/dream/detail"
     }
 
 }
